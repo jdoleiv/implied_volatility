99b4ffdf710045d6bdf795a25acbf5283e9fa795
John Dole <johndole1@Johns-MacBook-Air-3.local>
2016-09-09 12:03:41 +0800

    Test

--- R/european_options_data.R
+++ R/european_options_data.R
@@ -6,10 +6,10 @@
 #'
 #' @format A data frame with 20 observations and 5 variables:
 #' \describe{
-#'   \item{type}{One of the two values: 'call' or 'put' (string)}
-#'   \item{value}{Value of the option (number)}
-#'   \item{underlying}{Current price of the underlying (number)}
-#'   \item{strike}{Strike price of the option (number)}
-#'   \item{maturity}{Time to maturity in fractional years (number)}
+#'   \item{Type}{One of the two values: 'call' or 'put' (string)}
+#'   \item{Value}{Value of the option (number)}
+#'   \item{Underlying}{Current price of the underlying (number)}
+#'   \item{Strike}{Strike price of the option (number)}
+#'   \item{Maturity}{Time to maturity in fractional years (number)}
 #' }
 "european_options"
